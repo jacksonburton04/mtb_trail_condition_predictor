9bbc2a837c2b622682a46033063ba8c5fea2a9c2
jacksonburton04 <jacksonburton04@gmail.com>
2023-07-29 15:38:44 -0400

    update visual to be more readable

--- 04_visualize.py
+++ 04_visualize.py
@@ -17,12 +17,12 @@ colors = ["darkred", "red", "yellow", "lightgreen", "darkgreen"]
 cmap = ListedColormap(colors)
 norm = BoundaryNorm(boundaries, cmap.N, clip=True)
 
-fig, ax = plt.subplots(figsize=(15, 5))  # Adjust this to fit your display
+fig, ax = plt.subplots(figsize=(15, 7))  # Adjust this to fit your display
 
-plt.xticks(fontsize=8)  # Adjust the font size
-plt.yticks(fontsize=8)  # Adjust the font size
+plt.xticks(fontsize=10)  # Adjust the font size
+# plt.yticks(fontsize=10)  # Adjust the font size
 
-plt.title('Chance of Being Open by CORA Trail', fontsize=20, pad=20)  # Added pad=20
+plt.title('Chance of Being Open by CORA Trail', fontsize=30, pad=30)  # Added pad=20
 
 data = pivot_df.drop(columns=['Date'])
 
@@ -37,13 +37,17 @@ cbar.ax.tick_params(labelsize=8)
 for i in range(len(pivot_df['Date'])):
     for j in range(data.shape[1]):
         value = data.iloc[i, j]
-        text_color = "black" if value > 20 else "white"
-        plt.text(j + 0.5, i + 0.5, f'{value:.0f}%', ha='center', va='center', color=text_color, fontsize=6)  # Adjust the fontsize
+        text_color = "white" if value <= 10 or value >= 90 else "black"
+        plt.text(j + 0.5, i + 0.5, f'{value:.0f}%', ha='center', va='center', color=text_color, fontsize=9)  # Adjust the fontsize
 
 plt.xticks(rotation=45)
 plt.yticks(rotation=0)
 ax.set_yticks(range(len(pivot_df['Date'])))
-ax.set_yticklabels(pivot_df['Date'].dt.strftime('%Y-%m-%d'), fontsize=8)  # Adjust the fontsize
+ax.set_yticklabels(pivot_df['Date'].dt.strftime('%Y-%m-%d'), fontsize=11)  # Adjust the fontsize
+
+# Save the figure
+filename = 'daily_trail_condition_predictions.png'
+import boto3
 
 # Save the figure
 filename = 'daily_trail_condition_predictions.png'
@@ -52,4 +56,8 @@ fig.savefig(filename, dpi=300, bbox_inches='tight')
 # Upload the image to S3
 bucket_name = 'mtb-trail-condition-predictions'  # Change this to your bucket name
 s3 = boto3.client('s3')
-s3.upload_file(filename, bucket_name, filename)
+
+# Set the ACL to 'public-read' to allow public access
+s3.upload_file(filename, bucket_name, filename, ExtraArgs={'ACL': 'public-read'})
+
+print("Done! Check your S3 bucket for the image.")
